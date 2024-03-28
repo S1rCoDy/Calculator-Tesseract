253766ecc0cbc0a1e3068bd1387f4c62344dfcb3
S1rCoDy <148563516+S1rCoDy@users.noreply.github.com>
2024-03-28 13:05:21 +0500

    Beta 2.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,82 @@
+using Microsoft.Win32;
 using System.IO;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
 using System.Windows;
 using Tesseract;
+using System.Windows.Controls;
 
 namespace Calculator
 {
     public partial class MainWindow : Window
     {
-        // ... остальной код ...
+        private System.Drawing.Point _currentPoint;
+        private DispatcherTimer _timer;
+        private bool _isTabPressed;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _currentPoint = new System.Drawing.Point();
+            this.KeyDown += MainWindow_KeyDown;
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(2);
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    ImageBrush myBrush = new ImageBrush();
+                    Image image = new Image();
+                    image.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                    myBrush.ImageSource = image.Source;
+                    paintCanvas.Background = myBrush;
+                }
+            }
+            else if (e.Key == Key.Tab)
+            {
+                paintCanvas.Children.Clear();
+                paintCanvas.Background = Brushes.White;
+                _isTabPressed = true;
+            }
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                _currentPoint = new System.Drawing.Point((int)e.GetPosition(this).X, (int)e.GetPosition(this).Y);
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Line line = new Line
+                {
+                    Stroke = Brushes.Black,
+                    X1 = _currentPoint.X,
+                    Y1 = _currentPoint.Y,
+                    X2 = e.GetPosition(this).X,
+                    Y2 = e.GetPosition(this).Y,
+                    StrokeThickness = 8
+                };
+
+                _currentPoint = new System.Drawing.Point((int)e.GetPosition(this).X, (int)e.GetPosition(this).Y);
+
+                paintCanvas.Children.Add(line);
+            }
+        }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
